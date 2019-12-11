7608f7fb1116fa3f385b523bb5fb4b0aef00f421
Steven Williamson <steve@freeagent.com>
2019-12-11 20:15:35 +0000

    Add basic action file

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,21 @@ FROM golang:buster
 
 RUN apt-get update && apt-get -y install \
     git \
-    unzip;
+    unzip \
+    python-pip \
+    go-dep ;
+
+# Install gotestsum for parsing test output
+RUN curl -sSL "https://github.com/gotestyourself/gotestsum/releases/download/v0.4.0/gotestsum_0.4.0_linux_amd64.tar.gz" | tar -xz -C /usr/local/bin gotestsum
+
+# Install awscli
+RUN curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && \
+    unzip awscli-bundle.zip && \
+    ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws;
 
 RUN git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 RUN echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile && ln -s ~/.tfenv/bin/* /usr/local/bin
-RUN tfenv install 0.12.9 && tfenv install latest
+RUN tfenv install 0.12.9
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-echo "foo"
+set -eo pipefail
+
+GODIR=/go/src/github.com/fac
+
+# Setup SSH KEY for private repo access
+echo "Setting up SSH key"
+mkdir -p /root/.ssh && chmod 700 /root/.ssh
+echo "${INPUT_SSH_PRIV_KEY}" > /root/.ssh/id_rsa
+unset INPUT_SSH_PRIV_KEY
+
+chmod 600 /root/.ssh/id_rsa
+eval $(ssh-agent -s)
+ssh-add
+
+# Pre-seed host keys for github.com
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+
+# Make sure we have the correct terraform version, if we have a .terraform-version file
+tfenv install || true
+
+# Setup under GOPATH so dep etc works
+echo "Setting up GOPATH to include $PWD"
+CHECKOUT=$(basename "$PWD")
+mkdir -p $GODIR
+ln -s "$(pwd)" "${GODIR}/${CHECKOUT}"
+
+cd "${GODIR}/${CHECKOUT}/test"
+echo "Running go dep"
+dep ensure
+
+echo "Starting tests"
+gotestsum --format standard-verbose -- -v -timeout 50m -parallel 128
